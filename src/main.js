e1d506adbed294cd4ec65e4159593c4b5da5cd75
Ariel <ariel@cfee.gob.ar>
2022-04-29 15:09:49 -0300

    botón comprar y otros botones

--- src/main.js
+++ src/main.js
@@ -1,18 +1,60 @@
 
 
 
-var Kukis=3
+var Kukis=1;
+var Dinero=10;
+var Fer=2;
+
 function inicio(){
-  document.getElementById("cuantasKukis").innerHTML=Kukis;
-  }
+  document.getElementById("cuantoDinero").innerHTML=Dinero + " dineros";
+  if (Kukis == 1) {
+  document.getElementById("cuantasKukis").innerHTML=Kukis + " Kuki";
+} else {
+  document.getElementById("cuantasKukis").innerHTML=Kukis + " Kukis";
+}
+};
 
 function comerKuki() {
   if (Kukis >=1) {
   Kukis = Kukis -1;
 alert("Te comiste una Kuki. ¡Bien hecho!");
-document.getElementById("cuantasKukis").innerHTML=Kukis;
+
+if (Kukis == 1) {
+document.getElementById("cuantasKukis").innerHTML=Kukis + " Kuki";
 } else {
-  alert("No te quedan Kukis. ¡Bien!");}}
+document.getElementById("cuantasKukis").innerHTML=Kukis + " Kukis";
+}
+
+} else {
+  alert("No te quedan Kukis. ¡Bien!")
+  alert("¿debeías comprar más?")
+  document.getElementById("cajaCompra").style.visibility="visible";}
+};
+
+function comprarKuki() {
+  if (Fer == 2) {
+  Fer = 3;
+  alert("comprar cuesta dineros")
+  document.getElementById("botonCompra").innerHTML = "Comprar Kuki!";
+  document.getElementById("billetera").style.visibility="visible"
+  } else {
+  if (Dinero >=1) {
+  Dinero = Dinero -1
+  Kukis = Kukis +1;
+
+  if (Kukis == 1) {
+  document.getElementById("cuantasKukis").innerHTML=Kukis + " Kuki";
+} else {
+  document.getElementById("cuantasKukis").innerHTML=Kukis + " Kukis";
+}
+
+document.getElementById("cuantoDinero").innerHTML=Dinero + " dineros";
+} else {
+alert("No te queda más dinero. ¡Buenísimo!");
+document.getElementById("smith").style.visibility="visible"
+    }
+  };
+}
 
   function color(){
       rand1 = Math.floor(Math.random() * ((99- 10) + 1)) + 10
@@ -21,7 +63,11 @@ document.getElementById("cuantasKukis").innerHTML=Kukis;
       concatenado = rand1.toString() + rand2.toString() + rand3.toString()
       var color = "#" + concatenado
       document.body.style.background = color;
-      }
+    };
+
+  function argh() {
+      document.body.style.background = "#ffffff"
+    };
 
 
 /* LA FUNCIÓN DE BALLESTO let Numero = Number(prompt("Te comiste una Kuki"))
