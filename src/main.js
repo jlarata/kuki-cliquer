c192c3fbab192eb035f37e683adce486d097b4ee
jlarata <ariel@cfee.gob.ar>
2022-04-28 21:22:32 -0300

    : cambia color.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 
 
 
-
-
-
-
 var Kukis=3
 function inicio(){
   document.getElementById("cuantasKukis").innerHTML=Kukis;
@@ -17,6 +13,16 @@ alert("Te comiste una Kuki. ¡Bien hecho!");
 document.getElementById("cuantasKukis").innerHTML=Kukis;
 } else {
   alert("No te quedan Kukis. ¡Bien!");}}
+
+  function color(){
+      rand1 = Math.floor(Math.random() * ((99- 10) + 1)) + 10
+      rand2 = Math.floor(Math.random() * ((99- 10) + 1)) + 10
+      rand3 = Math.floor(Math.random() * ((99- 10) + 1)) + 10
+      concatenado = rand1.toString() + rand2.toString() + rand3.toString()
+      var color = "#" + concatenado
+      document.body.style.background = color;
+      }
+
 
 /* LA FUNCIÓN DE BALLESTO let Numero = Number(prompt("Te comiste una Kuki"))
 if (!Number.isNaN(Numero) && Numero != "") {
